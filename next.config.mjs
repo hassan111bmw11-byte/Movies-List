fb73f35a935641f0cba03c40a371596dd3f87162
Hassan <hassan111.bmw11@gmail.com>
2026-02-14 10:03:32 +0300

    add:pics path toconfig

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactCompiler: true,
   output: "export",
+
+  basePath: "/Movies-APP-List",
+  assetPrefix: "/Movies-APP-List/",
+
   images: {
     domains: ["m.media-amazon.com"],
     unoptimized: true,
