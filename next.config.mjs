7e7e9c4d83767dbec0116bf7401a832b4880407f
Hassan <hassan111.bmw11@gmail.com>
2026-02-14 09:52:34 +0300

    add:export in config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactCompiler: true,
+  output: "export",
   images: {
     domains: ["m.media-amazon.com"],
+    unoptimized: true,
   },
 };
 
